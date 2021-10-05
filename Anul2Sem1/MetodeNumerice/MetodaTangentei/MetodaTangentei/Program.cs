ce83750515107b82bcbac326074655bb62d99d35
PelleRemus <remusnicolaepelle@gmail.com>
2021-10-05 20:09:21 +0300

    Laboratorul 1 Metode Numerice

---
+++ Anul2Sem1/MetodeNumerice/MetodaTangentei/MetodaTangentei/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace MetodaTangentei
+{
+    class Program
+    {
+        static decimal a, b, dda, c, k;
+        static Func<decimal, decimal> F, DF;
+
+        static void Main(string[] args)
+        {
+            Ex1();
+            Console.WriteLine();
+            Ex2();
+            Console.WriteLine();
+            Ex3();
+        }
+
+        static void Ex1()
+        {
+            a = 1; b = 2;
+            dda = 6;
+            F = x => x*x*x - x - 1;
+            DF = x => 3*x*x - 1;
+
+            Console.WriteLine("\t\tf = x^3 - x - 1");
+            Console.WriteLine("\t\tf' = 3x^2 - 1");
+            MetodaTangentei(1e-4M);
+            MetodaTangentei(1e-8M);
+            MetodaTangentei(1e-12M);
+        }
+
+        static void Ex2()
+        {
+            c = 2;
+            b = c > 1 ? c : 1;
+            k = 2;
+            F = x => Power(x, k) - c;
+            DF = x => k * Power(x, k - 1);
+
+            Console.WriteLine("\t\tf = x^k - c");
+            Console.WriteLine("\t\tf' = kx^(k-1)");
+            MetodaTangentei(1e-4M);
+            MetodaTangentei(1e-8M);
+            MetodaTangentei(1e-12M);
+        }
+
+        static void Ex3()
+        {
+            b = 2;
+            k = 2;
+            F = x => x * (x*x + 3*b) / (3*x*x + b);
+            DF = null;
+
+            Console.WriteLine("\t\tf = x(x^2 + 3a) / (3x^2 + a)");
+            MetodaTangentei(1e-4M);
+            MetodaTangentei(1e-8M);
+            MetodaTangentei(1e-12M);
+        }
+
+        static void MetodaTangentei(decimal eps)
+        {
+            decimal xn_1 = F(a) * dda > 0 ? a : b;
+            decimal xn = DF == null ? F(xn_1) : xn_1 - F(xn_1) / DF(xn_1);
+            int n = 1;
+
+            while (Math.Abs(xn - xn_1) >= eps)
+            {
+                n++;
+                xn_1 = xn;
+                xn = DF == null ? F(xn_1) : xn_1 - F(xn_1) / DF(xn_1);
+            }
+            Console.WriteLine("n = {0} iteratii: xn = {1} (epsilon = {2})", n, xn, eps);
+        }
+
+        static decimal Power(decimal x, decimal k)
+        {
+            return (decimal)Math.Pow((double)x, (double)k);
+        }
+    }
+}
